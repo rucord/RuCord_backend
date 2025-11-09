f172f5fc0e948fe3c96f361e5638b1c2a3e5dfab
d0m-1k <d0m_1k@mail.ru>
2025-11-09 17:01:55 +0500

    fix routes

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,3 @@ def login(): return render_template("login.html")
 
 @app.route("/register", methods=["GET"])
 def register(): return render_template("register.html")
-
-@app.route("/register", methods=["GET"])
-def register(): return render_template("register.html")
