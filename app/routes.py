1ddf81807ef77b5b7709134fe3098673de996bb0
d0m-1k <d0m_1k@mail.ru>
2025-11-09 16:58:09 +0500

    update routes

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 from app import app
 from flask import render_template
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/", methods=["GET"])
+def index(): return render_template("index.html")
 
 @app.route("/login", methods=["GET"])
-def login():
-    return render_template("login.html")
+def login(): return render_template("login.html")
 
 @app.route("/register", methods=["GET"])
-def register():
-    return render_template("register.html")
+def register(): return render_template("register.html")
+
+@app.route("/register", methods=["GET"])
+def register(): return render_template("register.html")
